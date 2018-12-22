6208369eaa9b5dea8975c55dce6fc855239c1640
Ariel <arielord@buffalo.edu>
2018-12-22 03:46:45 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -28,27 +28,27 @@ class Owner
   end
   
   def buy_cat(name)
-    self.pets[cats] << Cat.new(name)
+    self.pets[:cats] << Cat.new(name)
   end
   
   def buy_fish(name)
-    self.pets[fishes] << Fish.new(name)
+    self.pets[:fishes] << Fish.new(name)
   end
   
   def buy_dog(name)
-    self.pets[dogs] << Dog.new(name)
+    self.pets[:dogs] << Dog.new(name)
   end
   
   def walk_dogs
-    self.pets[dogs] = self.pets[dogs].collect {|dog| dog.mood = "happy"}
+    self.pets[:dogs] = self.pets[dogs].collect {|dog| dog.mood = "happy"}
   end
   
   def play_with_cats
-    self.pets[cats] = self.pets[cats].collect {|cat| cat.mood = "happy"}
+    self.pets[:cats] = self.pets[cats].collect {|cat| cat.mood = "happy"}
   end
   
   def feed_fish
-    self.pets[fish] = self.pets[fishes].collect {|fish| fish.mood = "happy"}
+    self.pets[:fish] = self.pets[fishes].collect {|fish| fish.mood = "happy"}
   end
   
   def sell_pets
